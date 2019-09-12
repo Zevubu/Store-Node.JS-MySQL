cb66255b8951cba6b5802d3c7998b61d5b71cc3e
Zev Hoffman <zevubu@gmail.com>
2019-09-11 23:55:22 -0700

    got my show all products function working. also made place holders for other functions.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 let Mysql = require("mysql");
 let Inquirer = require("inquirer");
-let Keys = require("./keys")
+let Keys = require("./keys");
 
 let connection = Mysql.createConnection({
     host:"localhost",
@@ -9,9 +9,64 @@ let connection = Mysql.createConnection({
     password:Keys.SQLKEY,
     database:"bamazonDB"
 
-})
+});
+
+function allSearch(){
+    connection.query("SELECT * FROM products", function(err, res){
+        if(err) throw err;
+        console.log(res)
+    })
+}
+
+function nameSearch(){
+    productSearch();
+};
+
+function idSearch(){
+    productSearch();
+};
+
+function departmentSearch(){
+    productSearch();
+};
+
+function productSearch(){
+    Inquirer
+        .prompt({
+            name:"search",
+            type: "list",
+            message:"\n Welcome to The Hats, Mats, Rats, & Cats Emporium! \n What can we help you find?",
+            choices:[
+                "Show all products.",
+                "Search for product by ID.",
+                "Search for product by name.",
+                "Search for product by department.",
+                "Exit"
+            ]
+        })
+        .then(function(input){
+            switch(input.search){
+                case "Show all products.":
+                    allSearch();
+                    break;
+                case "Search for product by name.":
+                    nameSearch();
+                    break;
+                case "Search for product by ID.":
+                    idSearch();
+                    break;
+                case "Search for product by department.":
+                    departmentSearch();
+                    break;
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        });
+};
 
 connection.connect(function(err){
     if(err) throw err;
     console.log(`Connection thread id ${connection.threadId}`)
+    productSearch();
 })
