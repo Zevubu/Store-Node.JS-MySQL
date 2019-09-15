c65004cd32cb7d0f47f9930edd65d068a1e805c3
Zev Hoffman <zevubu@gmail.com>
2019-09-15 15:43:24 -0600

    add search by name and item id

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,7 @@ let Mysql = require("mysql");
 let Inquirer = require("inquirer");
 let Keys = require("./keys");
 
+let query;
 let connection = Mysql.createConnection({
     host:"localhost",
     port:3306,
@@ -23,11 +24,46 @@ function allSearch(){
 }
 
 function nameSearch(){
-    productSearch();
+    console.log("name check")
+    Inquirer
+        .prompt({
+            name:"item_name",
+            type:"input",
+            message:"\nWhat item would you like to search for? \n(try: floofy, top hat, Big hat, old mat, greasy.) "
+        })
+        .then(function(answer){
+            console.log(answer.item_name);
+            connection.query("SELECT * FROM products WHERE ?", {product_name: answer.item_name}, function(err, res){
+                if(err) throw err;
+                for (i = 0; i < res.length;i++){
+                    console.log(`\nID:${res[i].item_id}. Item Name: ${res[i].product_name}. Department: ${res[i].department_name}. Price: ${res[i].price}. Quantity: ${res[i].stock_quantity}. \n`)
+                }
+                
+            })
+            productSearch();  
+        })
 };
 
 function idSearch(){
-    productSearch();
+
+    console.log("name check")
+    Inquirer
+        .prompt({
+            name:"item_id",
+            type:"input",
+            message:"\nPlease enter the id number of the item you would like to see. \n"
+        })
+        .then(function(answer){
+            console.log(answer.item_id);
+            connection.query("SELECT * FROM products WHERE ?", {item_id: answer.item_id}, function(err, res){
+                if(err) throw err;
+                for (i = 0; i < res.length;i++){
+                    console.log(`\nID:${res[i].item_id}. Item Name: ${res[i].product_name}. Department: ${res[i].department_name}. Price: ${res[i].price}. Quantity: ${res[i].stock_quantity}. \n`)
+                }
+                
+            })
+            productSearch();  
+        })
 };
 
 function departmentSearch(){
@@ -39,7 +75,7 @@ function productSearch(){
         .prompt({
             name:"search",
             type: "list",
-            message:"\n Welcome to The Hats, Mats, Rats, & Cats Emporium! \n What can we help you find?",
+            message:"\nWelcome to The Hats, Mats, Rats, & Cats Emporium! \n What can we help you find?",
             choices:[
                 "Show all products.",
                 "Search for product by ID.",
